7a916f00e453f952607a68741543ab1d1e2b1b81
Dmitry Bondarev <tgenman@gmail.com>
2018-09-12 13:10:16 +0500

    Spring start initialization with maven

---
+++ src/main/java/com/dmitrybondarev/twidder/TwidderApplication.java
@@ -0,0 +1,12 @@
+package com.dmitrybondarev.twidder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwidderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwidderApplication.class, args);
+	}
+}
