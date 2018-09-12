d62acb33e8381d28cd4205197b48ca7ab7ff1894
Dmitry Bondarev <tgenman@gmail.com>
2018-09-12 19:46:48 +0500

    Connect to Postgres and add filters

---
+++ src/main/java/com/dmitrybondarev/twidder/repo/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.dmitrybondarev.twidder.repo;
+
+import com.dmitrybondarev.twidder.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
